e883c3111ede80f68b6bd4b73edef9f9ea6887c9
Alexandre Haddad-Delaveau <ahaddaddelaveau@gmail.com>
2024-09-10 14:09:47 -0700

    Commit #2: See WORK_LOG.md

--- src/HighwaysAndHospitals.java
+++ src/HighwaysAndHospitals.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -8,7 +9,6 @@ import java.util.Queue;
  * at Menlo School in Atherton, CA
  *
  * Completed by: Alexandre Haddad-Delaveau
- *
  */
 
 public class HighwaysAndHospitals {
@@ -20,15 +20,30 @@ public class HighwaysAndHospitals {
 
     // Keep track of visited cities
     boolean[] visited;
+    boolean[] hasHospital;
 
     // Constructor
     public HighwaysAndHospitals(int n, int hospitalCost, int highwayCost, int[][] cities) {
         this.n = n;
         this.hospitalCost = hospitalCost;
         this.highwayCost = highwayCost;
-        this.cities = cities;
+        visited = new boolean[n + 1];
+        hasHospital = new boolean[n + 1];
 
-        visited = new boolean[n];
+        // Convert cities to new format
+        this.cities = new int[n + 1][];
+        ArrayList<Integer>[] citiesArrayList = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) citiesArrayList[i] = new ArrayList<>();
+
+        for (int[] edge : cities) {
+            citiesArrayList[edge[0]].add(edge[1]);
+            citiesArrayList[edge[1]].add(edge[0]);
+        }
+
+        // Convert to array
+        for (int i = 1; i <= n; i++) {
+            this.cities[i] = citiesArrayList[i].stream().mapToInt(j -> j).toArray();
+        }
     }
 
     public long cost() {
@@ -37,8 +52,10 @@ public class HighwaysAndHospitals {
         // Else, add hospital and destroy neighboring hospitals
         // When complete, return total cost
 
-        // Total Cost
-        int totalCost = 0;
+        // Quick bypass for hospitals cost >= highways cost
+        if (hospitalCost <= highwayCost) {
+            return (long) n * hospitalCost;
+        }
 
         // Queue for BFS
         Queue<Integer> queue = new LinkedList<>();
@@ -46,28 +63,47 @@ public class HighwaysAndHospitals {
         // Loop until all cities have been visited
         while (true) {
             // Check if there are any cities left to visit
-            int initialCity = findFirstCityWithoutHospital();
-            if (initialCity == -1) {
-                break;
-            }
+            int initialCity = findFirstUnvisitedCity();
+            if (initialCity == -1) break;
 
             // Add initial city to queue
             queue.add(initialCity);
             visited[initialCity] = true;
-            totalCost += hospitalCost;
+            hasHospital[initialCity] = true;
 
-            //
+            // Loop through neighbours
+            while (!queue.isEmpty()) {
+                // Get current city
+                int currentCity = queue.remove();
 
+                // Count new neighbours
+                int newNeighbours = 0;
+                for (int neighbour : cities[currentCity]) {
+                    if (!visited[neighbour]) {
+                        newNeighbours++;
 
+                        // Also add to queue & mark visited
+                        queue.add(neighbour);
+                        visited[neighbour] = true;
+                    }
+                }
 
+                // Decide if a hospital or neighbor is cheaper
+                if (buildHospital(newNeighbours)) {
+                    hasHospital[currentCity] = true;
+
+                    // If a hospital is built, destroy neighboring hospitals (if hopsital cost > highway cost)
+                    for (int neighbour : cities[currentCity]) {
+                        hasHospital[neighbour] = false;
+                    }
+                }
+            }
         }
-
-
-        return 0;
+        return calculateCost();
     }
 
-    private int findFirstCityWithoutHospital() {
-        for (int i = 0; i < n; i++) {
+    private int findFirstUnvisitedCity() {
+        for (int i = 1; i <= n; i++) {
             if (!visited[i]) {
                 return i;
             }
@@ -76,6 +112,21 @@ public class HighwaysAndHospitals {
     }
 
     private boolean buildHospital(int neighbours) {
-        return neighbours * highwayCost > hospitalCost;
+        return neighbours * highwayCost > hospitalCost || highwayCost >= hospitalCost;
+    }
+
+    private long calculateCost() {
+        // Calculate cost of building hospitals and highways
+        // (assuming each city must either be connected by a highway or have a hospital)
+        long totalCost = 0;
+        for (int i = 1; i <= n; i++) {
+            if (hasHospital[i]) {
+                totalCost += hospitalCost;
+            } else {
+                totalCost += highwayCost;
+            }
+        }
+
+        return totalCost;
     }
 }
